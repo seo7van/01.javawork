19243f536e79dd51308fdd466266f6154278ea7b
seojinchoiiii <qkzkel17@naver.com>
2024-05-07 14:36:36 +0900

    2024.05.07_1day
    println, print

---
+++ day01/src/day01/T01_print.java
@@ -0,0 +1,28 @@
+package day01;
+
+public class T01_print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * rtgfgfgfgfgrrgrgrkgifgjkiofjgpokr
+		 */
+		System.out.println("더조은 컴퓨터 학원");
+		System.out.println("내이름 : 최서진");
+System.out.println("연산결과 : " + 7+9);
+System.out.println("연산결과: " + 7+9);
+System.out.println("연산결과: " + 7*9);
+
+System.out.print("줄바꿈 안함");
+System.out.print("이제 줄바꿈 안됨");
+
+System.out.print("저는 최서진");
+System.out.print(");
+}}
